71c01c95195b33c3f68072335dfcbf8610d0cd13
Jonathan Mason <Jon Mason>
2015-08-17 14:59:46 -0500

    fix spec to reflect changes in annotating behavior, add cases

--- spec/roles_spec.rb
+++ spec/roles_spec.rb
@@ -10,7 +10,6 @@ describe Conjur::Ldap::Roles do
     {
         save_api_keys: false,
         ignore_ldap_ids: true,
-        marker_tag: 'ImportedFrom=LDAP',
         owner: roleid
     }
   end
@@ -19,8 +18,8 @@ describe Conjur::Ldap::Roles do
   let(:resource){ double('resource', annotations: annotations) }
   let(:conjur_user){ double('conjur user', :exists? => false, resource: resource) }
   let(:conjur_group){ double('conjur group', :exists? => false, resource: resource) }
-  let(:group){ double('group', name: 'users', dn: nil, gid: 1234, members: []) }
-  let(:user){ double('user',name: 'alice', dn: nil, uid: 123, groups: []) }
+  let(:group){ double('group', name: 'users', dn: 'cn=users,dc=conjur,dc=net', gid: 1234, members: []) }
+  let(:user){ double('user',name: 'alice', dn: 'cn=alice,dc=conjur,dc=net', uid: 123, groups: []) }
   let(:users){ [user] }
   let(:groups){ [group] }
   let(:model) do
@@ -37,7 +36,6 @@ describe Conjur::Ldap::Roles do
     allow(subject).to receive(:group).with(group.name).and_return(conjur_group)
     allow(subject).to receive(:create_user).with(user.name, ownerid: roleid).and_return(conjur_user)
     allow(subject).to receive(:create_group).with(group.name, ownerid: roleid).and_return(conjur_group)
-    allow(annotations).to receive(:[]=).with 'ImportedFrom', 'LDAP'
   end
 
   shared_context 'no updates' do
@@ -52,8 +50,34 @@ describe Conjur::Ldap::Roles do
     include_context 'no updates'
 
     it 'adds them' do
-      expect(annotations).to receive(:[]=).with('Foo','BAR').at_least(:once)
-      subject.sync_to model, base_opts.merge(marker_tag: 'Foo=BAR')
+      expect(annotations).to receive(:merge!).with(
+                                 'ldap-sync/source' => 'blah',
+                                 'ldap-sync/upstream-dn' => group.dn
+                             )
+      expect(annotations).to receive(:merge!).with(
+                                 'ldap-sync/source' => 'blah',
+                                 'ldap-sync/upstream-dn' => user.dn
+                             )
+      subject.sync_to model, base_opts.merge(marker_tag: 'blah')
+    end
+
+    context 'with no :marker_tag option' do
+      let(:current_roleid){ 'current-role-id' }
+      let(:current_role){ double('current role', roleid: current_roleid) }
+
+      before do
+        allow(subject).to receive(:current_role).and_return current_role
+      end
+
+      it 'sets the ldap-sync/source annotation to the current role id' do
+        [user, group].each do |role|
+          expect(annotations).to receive(:merge!).with(
+                                     'ldap-sync/source' => current_roleid,
+                                     'ldap-sync/upstream-dn' => role.dn
+                                 )
+        end
+        subject.sync_to model, base_opts
+      end
     end
   end
 end
